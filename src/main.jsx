db82a8eb4bef586e9928a5a0c64b4e3fa8af9a8a
m.bagus shokhibul khikham <semenkakitiga@gmail.com>
2026-06-13 14:38:52 +0700

    Fix routing untuk GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'  // ← Pastikan import ini ada!
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+// Get the base path from the URL
+const basename = '/sistem-absensi'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>  {/* ← Pastikan ini ada! */}
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
